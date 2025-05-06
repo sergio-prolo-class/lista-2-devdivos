bbede7147d913f9d75be985490cfd8eced7d13e1
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2025-05-06 19:15:01 -0300

    Criado grade.java

---
+++ app/src/main/java/ifsc/poo/Grade.java
@@ -0,0 +1,13 @@
+package ifsc.poo;
+import edu.princeton.cs.algs4.Draw;
+public class Grade {
+    private final int x;
+    private final int y;
+    private final int tamanhoCelula = 40; // tamanho de cada célula da grade
+
+    public Grade(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
